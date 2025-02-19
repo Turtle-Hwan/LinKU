11318ca4ea36ab7fd3f87c61a413a5de56b10584
Turtle-Hwan <kjhwan0802@naver.com>
2025-02-20 02:09:11 +0900

    feat: install shad/cn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // base를 상대경로로 설정
   base: "",
   build: {
