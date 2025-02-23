40ef739426bd2cab486ce8412905e146ce109f15
kim jihwan <kjhwan0802@naver.com>
2025-02-23 23:05:05 +0900

    fix: 권한 제거 및 alpha 버전

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ import "./App.css";
 import LinkGroup from "./components/LinkGroup";
 
 function App() {
-  useEffect(() => {
-    async function initializeExtension() {
-      const currentTab = await getCurrentTab();
-      if (!currentTab.id) return;
+  // useEffect(() => {
+  //   async function initializeExtension() {
+  //     const currentTab = await getCurrentTab();
+  //     if (!currentTab.id) return;
 
-      // await executeScriptFile(currentTab.id, ["getSource.js"]);
+  //     // await executeScriptFile(currentTab.id, ["getSource.js"]);
 
-      const refreshButton = document.getElementById("refresh-btn");
-      refreshButton?.addEventListener("click", async () => {
-        await executeScript(currentTab.id!, sugangRefreshBtn);
-      });
-    }
+  //     const refreshButton = document.getElementById("refresh-btn");
+  //     refreshButton?.addEventListener("click", async () => {
+  //       await executeScript(currentTab.id!, sugangRefreshBtn);
+  //     });
+  //   }
 
-    initializeExtension();
-  }, []);
+  //   initializeExtension();
+  // }, []);
 
   return (
     <>
