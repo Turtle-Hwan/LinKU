0105a8280da559a4b1688e3fffeace715764d032
kim jihwan <kjhwan0802@naver.com>
2025-02-27 02:44:04 +0900

    chore: add console log easter egg

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,12 @@ function App() {
   //   initializeExtension();
   // }, []);
 
+  console.log(
+    "%c여길 열어보시다니...\n이 참에 직접 코드 기여도 해주시는 건 어떤가요?",
+    "font-family: Nanum Gothic; color: darkgreen; padding: 6px; border-radius: 4px; font-size:14px"
+  );
+  console.log("https://github.com/Turtle-Hwan/LinKU");
+
   return (
     <>
       {/* <button id="refresh-btn">Refresh</button> */}
