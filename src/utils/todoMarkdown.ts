be970d0a8a23acc667a0ff6500d3817d737e6d7c
Turtle-Hwan <kjhwan0802@naver.com>
2025-10-10 02:22:39 +0900

    feat: add convertTodosToMarkdown function for converting Todo items to markdown checklist format

---
+++ src/utils/todoMarkdown.ts
@@ -0,0 +1,33 @@
+import { TodoItem } from "@/apis/eCampusAPI";
+
+/**
+ * Todo 항목들을 마크다운 체크리스트 형식으로 변환합니다.
+ *
+ * @param todos - 변환할 Todo 항목 배열
+ * @returns 마크다운 형식의 문자열
+ *
+ * @example
+ * ```typescript
+ * const todos = [
+ *   { title: "과제 제출", subject: "프로그래밍", dueDate: "2024-03-20", dDay: "D-3" }
+ * ];
+ *
+ * convertTodosToMarkdown(todos);
+ * // "- [ ] 과제 제출  |  프로그래밍 - 2024-03-20"
+ * ```
+ */
+export const convertTodosToMarkdown = (todos: TodoItem[]): string => {
+  if (todos.length === 0) {
+    return "할 일이 없습니다.";
+  }
+
+  return todos
+    .map((item) => {
+      const title = `${item.title}`;
+      const subject = `${item.subject}`;
+      const deadline = `${item.dueDate}`;
+
+      return `- [ ] ${title}  |  ${subject} - ${deadline}`;
+    })
+    .join("\n");
+};
